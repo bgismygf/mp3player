a6dcb61b117d0abe623ce1d472384e01da565ccb
zac <ggyy06059909@gmail.com>
2020-03-23 09:37:59 +0800

    測試、音樂資料輸入、加入秒數樣式

--- src/views/Mp3Player.vue
+++ src/views/Mp3Player.vue
@@ -1,20 +1,41 @@
 <template>
   <div>
+<!-- 左邊 - 音樂播放器 -->
     <div class="container mt-5">
       <div class="row justify-content-center">
         <div class="col-12 col-sm-8 col-md-6 col-lg-5">
-          <div class="bg-cover-c rounded-lg" style="background-image:url('https://upload.wikimedia.org/wikipedia/zh/0/0c/Niche_Syndrome_-_ONE_OK_ROCK_album_cover.png');"></div>
+          <audio id="audio">
+            <source id="s" src="../assets/music/WhereverYouAre.mp3" type="audio/mpeg" />
+          </audio>
+<!-- 專輯封面 -->
+          <div class="bg-cover-c rounded-lg"
+            style="background-image:url('https://i.kfs.io/album/global/21664553,0v1/fit/500x500.jpg');"></div>
+<!-- 音樂名稱 -->
           <h1 class="h3 text-center text-white mt-3">ONE OK ROCK</h1>
           <h2 class="h3 text-center text-white mt-1">Wherever You Are</h2>
-          <div class="text-center mt-4">
+<!-- 播放按鈕 -->
+          <div class="text-center mt-3">
             <a href="#"><i class="fas fa-step-backward text-white"></i></a>
-            <a v-if="false" class="text-white mx-5" href="#"><i class="fas fa-play"></i></a>
-            <a v-else class="mx-5" href="#"><i class="fas fa-pause text-white"></i></a>
+            <a v-if="!playSwitch" class="text-white mx-5" href="#"
+              @click.prevent="playOrStopSong(true)">
+              <i class="fas fa-play"></i>
+            </a>
+            <a v-else class="mx-5" href="#"
+              @click.prevent="playOrStopSong(false)">
+              <i class="fas fa-pause text-white"></i>
+            </a>
             <a href="#"><i class="fas fa-step-forward text-white"></i></a>
           </div>
-          <div class="w-100 mt-4 rounded-pill border">
+<!-- 時間 -->
+          <div class="d-flex justify-content-between">
+            <span class="text-white">00:00</span>
+            <span>00:00</span>
+          </div>
+<!-- 時間條 -->
+          <div class="w-100 mt-2 rounded-pill border">
             <div class="bg-white" style="height:5px; width: 70%;"></div>
           </div>
+<!-- 重播、音量、隨機 -->
           <div class="d-flex mt-4 justify-content-between w-100">
             <a href="#"><i class="fas fa-retweet text-white"></i></a>
             <div class="d-flex w-100 justify-content-center">
@@ -27,22 +48,14 @@
             <a href="#"><i class="fas fa-random text-white"></i></a>
           </div>
         </div>
+<!-- 右邊 - 音樂列表 -->
         <div class="col-12 col-sm-8 col-md-7 col-lg-5 mt-4 mt-lg-0">
               <div class="d-flex justify-content-between border-bottom border-white
-                pb-3 cursor-pointer mb-4">
+                pb-3 cursor-pointer mb-4" v-for="(item, index) in songList" :key="index">
               <span class="text-white">
-                1. ONE OK ROCK - Wherever You Are
+                {{ index + 1 }}. {{ item.title }} - {{ item.musicName }}
               </span>
               <a v-if="false" href="#"><i class="fas fa-play text-white"></i></a>
-              <a v-else href="#"><i class="fas fa-pause text-white"></i></a>
-            </div>
-
-            <div class="d-flex justify-content-between border-bottom border-white
-                pb-3 cursor-pointer mb-4">
-              <span class="text-white">
-                2. ONE OK ROCK - The Beginning
-              </span>
-              <a v-if="true" href="#"><i class="fas fa-play text-white"></i></a>
               <a v-else href="#"><i class="fas fa-pause text-white"></i></a>
             </div>
         </div>
@@ -54,6 +67,71 @@
 <script>
 export default {
   name: 'Mp3Player',
+  data() {
+    return {
+      playSwitch: false,
+      currentlyPlaying: '../assets/music/WhereverYouAre.mp3',
+      songList: [
+        {
+          title: 'ONE OK ROCK',
+          musicName: 'Wherever You Are',
+          fileName: 'WhereverYouAre',
+          img: 'https://i.kfs.io/album/tw/328901,0v3/fit/500x500.jpg',
+        },
+        {
+          title: 'ONE OK ROCK',
+          musicName: 'The Beginning',
+          fileName: '../assets/music/TheBeginning.mp3',
+          img: 'https://i.kfs.io/album/tw/746878,0v3/fit/500x500.jpg',
+        },
+        {
+          title: 'ONE OK ROCK',
+          musicName: 'Mighty Long Fall',
+          fileName: 'MightyLongFall',
+          img: 'https://i.kfs.io/album/global/8518438,0v1/fit/500x500.jpg',
+        },
+        {
+          title: 'ONE OK ROCK',
+          musicName: 'Cry Out',
+          fileName: 'CryOut',
+          img: 'https://i.kfs.io/album/global/8518438,0v1/fit/500x500.jpg',
+        },
+        {
+          title: 'ONE OK ROCK',
+          musicName: 'Bedroom Warfare',
+          fileName: 'BedroomWarfare',
+          img: 'https://i.kfs.io/album/global/21664553,0v1/fit/500x500.jpg',
+        },
+        {
+          title: 'ONE OK ROCK',
+          musicName: 'Remake',
+          fileName: 'Remake',
+          img: 'https://i.kfs.io/album/tw/328916,0v1/fit/500x500.jpg',
+        },
+      ],
+    };
+  },
+  methods: {
+    playOrStopSong(judge) {
+      const audio = document.querySelector('audio');
+      const s = document.querySelector('#s');
+      if (judge) {
+        console.log('1', audio.currentSrc);
+        console.log('1-1', s.currentSrc);
+        // s.src = '../assets/music/MightyLongFall.mp3';
+        // console.log('2', audio.currentSrc);
+        // console.log('2-1', s.currentSrc);
+        // console.log(audio.networkState);
+        // audio.load();
+        audio.play();
+        // console.log(audio.networkState);
+      } else {
+        audio.pause();
+      }
+      this.playSwitch = judge;
+      // console.log(this.$refs.s);
+    },
+  },
 };
 </script>
 
